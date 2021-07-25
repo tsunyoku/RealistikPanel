9cd4acfb1f00a09d0d3d88abc7ae23143f0f859d
tsunyoku <tsunyoku@gmail.com>
2021-07-25 03:10:32 +0000

    prepare repo

---
+++ ext/config.sample.py
@@ -0,0 +1,15 @@
+sql = {
+    'host': 'localhost',
+    'db': 'asahi',
+    'user': 'tsunyoku',
+    'password': 'lol123'
+}
+
+redis = {
+    'host': 'localhost',
+    'db': 0,
+    'password': ''
+}
+
+socket = '/tmp/asahipanel.sock'
+domain = 'admin.tsunyoku.xyz'
---
+++ objects/glob.py
@@ -0,0 +1,10 @@
+import config
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from cmyui import Version
+    from fatFuckSQL import fatFawkSQL
+
+version: 'Version'
+db: 'fatFawkSQL'
